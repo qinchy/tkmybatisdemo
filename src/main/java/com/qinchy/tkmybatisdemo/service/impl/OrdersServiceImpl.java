01fcca7ff3d3a93ffbf514cc4aec249a407318e8
qinchy <qinchy@msn.com>
2017-11-03 15:10:40 +0800

    添加gitignore文件

--- src/main/java/com/qinchy/tkmybatisdemo/service/impl/OrdersServiceImpl.java
+++ src/main/java/com/qinchy/tkmybatisdemo/service/impl/OrdersServiceImpl.java
@@ -2,6 +2,7 @@ package com.qinchy.tkmybatisdemo.service.impl;
 
 import com.qinchy.tkmybatisdemo.dao.OrdersMapper;
 import com.qinchy.tkmybatisdemo.model.Orders;
+import com.qinchy.tkmybatisdemo.model.OrdersExample;
 import com.qinchy.tkmybatisdemo.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class OrdersServiceImpl implements OrdersService {
 
     @Override
     public List<Orders> findAll() {
-        return ordersMapper.selectAll();
+        OrdersExample ordersExample = new OrdersExample();
+        return ordersMapper.selectByExample(ordersExample);
     }
 }
