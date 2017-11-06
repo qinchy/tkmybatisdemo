924c4e39141484eb388868b7e5a5f15dde759a70
qinchy <qinchy@msn.com>
2017-11-06 16:23:45 +0800

    添加gitignore文件

--- src/main/java/com/qinchy/tkmybatisdemo/controller/OrdersController.java
+++ src/main/java/com/qinchy/tkmybatisdemo/controller/OrdersController.java
@@ -1,9 +1,13 @@
 package com.qinchy.tkmybatisdemo.controller;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.qinchy.tkmybatisdemo.model.Orders;
 import com.qinchy.tkmybatisdemo.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +21,12 @@ public class OrdersController {
     @GetMapping(path = "/orders/findAll")
     public List<Orders> findAll(){
         return ordersService.findAll();
+    }
+
+    @GetMapping(path = "/orders/findByPage")
+    public PageInfo<Orders> findByPage(@RequestParam("pageNum") Integer pageNum,@RequestParam("pageSize") Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<Orders> orders = ordersService.findAll();
+        return new PageInfo<Orders>(orders);
     }
 }
